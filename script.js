88ec6d84b0c2b2d2166403c7c09eb0b6142ea561
Miguel Ryan Dantas de Freitas <73431866+athavus@users.noreply.github.com>
2023-10-21 22:47:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function socialMedia(name) {
+    const id = name;
+    if (id == 'instagram') {
+        window.open("https://www.instagram.com/1mrzin/")
+    } else if (id == 'linkedin') {
+        window.open("https://www.linkedin.com/in/miguel-ryan-freitas-914504245/")
+    } else if (id == 'whatsapp') {
+        window.open("https://wa.me/5583988864397")
+    } else if (id == 'github') {
+        window.open("https://github.com/athavus")
+    }
+}
+
+function abrir(link) {
+    window.open(link);
+}
