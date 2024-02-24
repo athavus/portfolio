eae10ddf110244f7721bbc25f0da591ac228680f
Miguel Ryan Dantas de Freitas <miguelryanfreitas@gmail.com>
2024-02-24 20:43:59 -0300

    add animations to all webpage

--- script.js
+++ script.js
@@ -14,3 +14,46 @@ function socialMedia(name) {
 function abrir(link) {
     window.open(link);
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    function checkVisibility(entries, observer) {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('triggered');
+                observer.unobserve(entry.target); 
+            }
+        });
+    }
+    var observer = new IntersectionObserver(checkVisibility);
+
+    var boxes = document.querySelectorAll('.sec1');
+    boxes.forEach(box => {
+        observer.observe(box);
+    });
+
+    var myCertificates = document.querySelectorAll('.myCertificates')
+    myCertificates.forEach(certificate => {
+        observer.observe(certificate);
+    })
+
+    var myCompletedProjects = document.querySelectorAll('.myCompletedProjects')
+    myCompletedProjects.forEach(projects => {
+        observer.observe(projects);
+    }) 
+    var myOnGoingProjects = document.querySelectorAll('.myOnGoingProjects')
+    myOnGoingProjects.forEach(projects => {
+        observer.observe(projects);
+    })
+
+    document.getElementById('acessarLajedo').addEventListener('click', function() {
+        var url = './documents/patentelajedo.pdf';
+        var filename = 'Patente - Lajedo do Marinho.pdf';
+        var link = document.createElement('a');
+        link.style.display = 'none';
+        document.body.appendChild(link);
+        link.setAttribute('href', url);
+        link.setAttribute('download', filename);
+        link.click();
+        document.body.removeChild(link);
+    });
+});
